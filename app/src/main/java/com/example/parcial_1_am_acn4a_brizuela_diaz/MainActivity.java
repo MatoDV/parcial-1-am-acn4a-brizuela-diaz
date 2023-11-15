673ba8daec9ec83e74892b3e8e8b178b0532134b
Matias Brizuela <115161150+MatoDV@users.noreply.github.com>
2023-11-15 11:03:05 -0300

    feat: create onclick listener for the imagebuttons and change the names for avoid problems

--- app/src/main/java/com/example/parcial_1_am_acn4a_brizuela_diaz/MainActivity.java
+++ app/src/main/java/com/example/parcial_1_am_acn4a_brizuela_diaz/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.parcial_1_am_acn4a_brizuela_diaz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +17,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton btnInstagram = findViewById(R.id.img_instagram);
+        btnInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ImageButtonClick", "Instagram button clicked");
+                String url = "https://www.instagram.com/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.setPackage("com.android.chrome"); // Paquete de Chrome
+                startActivity(intent);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        ImageButton btnFacebook = findViewById(R.id.img_facebook);
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ImageButtonClick", "Facebook button clicked");
+                String url = "https://www.facebook.com/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.setPackage("com.android.chrome"); // Paquete de Chrome
+                startActivity(intent);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                Log.e("ImageButtonClick", "No se puede abrir el enlace");
+            }
+            }
+        });
+        ImageButton btnGmail = findViewById(R.id.img_gmail);
+        btnGmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ImageButtonClick", "Gmail button clicked");
+                String url = "https://mail.google.com/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.setPackage("com.android.chrome"); // Paquete de Chrome
+                startActivity(intent);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
